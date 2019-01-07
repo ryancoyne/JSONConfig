613cd4e9e067589e158b4daa5297c46da481ae3d
Ryan <ryan@buckettechnologies.com>
2019-01-06 19:15:29 -0500

    Trying to see why JSONCOnfig isnt being seen by another module

--- Package.swift
+++ Package.swift
@@ -7,16 +7,17 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
            name: "JSONConfig",
-           targets: ["JSONConfig"]),
+           targets: ["JSONConfig"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/ryancoyne/PerfectLib.git", from: "4.0.0"),
     ],
     targets: [
         .target(
-	   name: "JSONConfig",
-	   dependencies: ["PerfectLib"],
-	   path: "Sources"
-	),
+            name: "JSONConfig",
+            dependencies: ["PerfectLib"],
+            path: "Sources"
+        ),
     ]
 )
